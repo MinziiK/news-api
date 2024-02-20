a12f9f102f38ab39ffe8d1c9798929fdfcb6e165
MinziiK <zxcv923111@gmail.com>
2024-02-20 23:35:11 +0900

    second commit

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const getLatestNews = async ()=>{
     const url = new URL(
         `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
         ); 
-    // console.log("uuu", url);
+    console.log("uuu", url);
 
     const response = await fetch(url);    // url 호출 함수 : fetch
     const data = await response.json();
     news = data.articles;
 
-    // console.log("rrr", response);
-    // console.log("data", data)
-    // console.log("articles", news);
+    console.log("rrr", response);
+    console.log("data", data)
+    console.log("articles", news);
 }
 
 getLatestNews();
